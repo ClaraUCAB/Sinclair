c9e817949edc58a5ab3d57b1f8d22f3467532b8b
ClaraUCAB <146204299+ClaraUCAB@users.noreply.github.com>
2026-01-10 01:59:50 -0400

    Forgor to push AuthHandler.ts

---
+++ src/handlers/AuthHandler.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { AuthService } from "../services/AuthService.ts";
+import { ApiResponse } from "../types/index.ts";
+
+export class AuthHandler {
+    constructor(private service: AuthService) {}
+
+    async register(req: Request, res: Response) {
+        res
+            .setHeader('Authorization', 'Bearer waos')
+            .setHeader('X-Que', 'So')
+            .type('application/json')
+            .send('{"waos": 11037}');
+    }
+
+    async login(req: Request, res: Response) {
+        res
+            .setHeader('Authorization', 'Bearer waos')
+            .setHeader('X-Que', 'So')
+            .type('application/json')
+            .send('{"waos": 11037}');
+    }
+}
+
