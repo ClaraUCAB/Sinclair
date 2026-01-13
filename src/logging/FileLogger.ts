d1931517c04d6944920c4fe83181c189f808c968
ClaraUCAB <146204299+ClaraUCAB@users.noreply.github.com>
2026-01-13 14:40:25 -0400

    Remove debug log

--- src/logging/FileLogger.ts
+++ src/logging/FileLogger.ts
@@ -6,7 +6,6 @@ export class FileLogger implements ILogger {
 	private readonly FILE_PATH = path.join(process.cwd(), '/file.log');
 	async log(entry: LogEntry): Promise<void> {
 		try {
-			console.log(this.FILE_PATH);
 			await fs.appendFile(this.FILE_PATH, JSON.stringify(entry) + '\n', 'utf8');
 		} catch (error) {
 			throw new error();
